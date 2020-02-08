f4cc8b529ebbb1428b4528d6348feed7fd850d37
pepo4343 <ptangwongkit@gmail.com>
2020-02-08 10:00:49 +0700

    add dashboard

--- nbiotplatform/udpserver/app.js
+++ nbiotplatform/udpserver/app.js
@@ -1,18 +1,42 @@
 const dgram = require("dgram");
-const server = dgram.createSocket("udp4");
+const serverudp = dgram.createSocket("udp4");
 
-server.on("error", err => {
+const express = require("express");
+const http = require("http");
+
+const port = process.env.PORT || 4001;
+
+const app = express();
+app.use((req, res, next) => {
+  res.send("hello");
+});
+
+const serversocket = http.createServer(app);
+const io = require("./socket").init(serversocket);
+var numClient = 0;
+io.on("connection", socket => {
+  numClient++;
+  io.emit("numclient", { numClient });
+  console.log("Client Connected :", numClient);
+  socket.on("disconnect", () => {
+    numClient--;
+    console.log("Client Disconnected : ", numClient);
+  });
+});
+serversocket.listen(port, () => console.log(`Listening on port ${port}`));
+serverudp.on("error", err => {
   console.log(`server error:\n${err.stack}`);
   server.close();
 });
 
-server.on("message", (msg, rinfo) => {
+serverudp.on("message", (msg, rinfo) => {
   console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+  io.emit("message", { msg: msg.toString() });
 });
 
-server.on("listening", () => {
-  const address = server.address();
+serverudp.on("listening", () => {
+  const address = serverudp.address();
   console.log(`server listening ${address.address}:${address.port}`);
 });
 
-server.bind(41234);
+serverudp.bind(41234);
