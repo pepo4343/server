1ba373639c4dcc0d91802e35a4c6a8ccd438738f
pepo4343 <ptangwongkit@gmail.com>
2020-02-08 10:04:33 +0700

    add build

---
+++ nbiotplatform/dashboard/build/precache-manifest.586c3c64034d717452681e8858b1da15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457641daef73cd655a6341ac97773166",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9df4f7189e93b85b3e84",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8a867c620e2e53a9540b",
+    "url": "/static/js/2.cd84a243.chunk.js"
+  },
+  {
+    "revision": "222b4a5f5cd655e34e2a29e8aabff23f",
+    "url": "/static/js/2.cd84a243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9df4f7189e93b85b3e84",
+    "url": "/static/js/main.38c72e62.chunk.js"
+  },
+  {
+    "revision": "425d6768d8d217c2ae35",
+    "url": "/static/js/runtime-main.b4e92301.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
