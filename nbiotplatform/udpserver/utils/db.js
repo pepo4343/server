9720e21bc300c1f8c05c4103043323c6d583d591
pepo4343 <ptangwongkit@gmail.com>
2020-02-11 22:53:19 +0700

    test

--- nbiotplatform/udpserver/utils/db.js
+++ nbiotplatform/udpserver/utils/db.js
@@ -8,9 +8,8 @@ let db;
 const mongoConnect = async (db_name, Callback) => {
   try {
     const client = await mongo_client.connect(url);
-
-    Callback();
     db = client.db(db_name);
+    Callback();
   } catch (e) {
     throw e;
   }
