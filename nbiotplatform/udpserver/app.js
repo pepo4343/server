ce7e11debd4e8cff5630bff4ae3ea1c78aa791ee
pepo4343 <ptangwongkit@gmail.com>
2020-07-12 20:36:46 +0700

    edit app.js

--- nbiotplatform/udpserver/app.js
+++ nbiotplatform/udpserver/app.js
@@ -55,6 +55,7 @@ serverudp.on("error", (err) => {
 serverudp.on("message", (msg, rinfo) => {
   console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
   const json = JSON.parse(msg);
+  console.log(json);
   if(json.type == "PM"){
     if(json.dev_id==="0001"){ //nbiot
       console.log(json);
