84d4e7e32d43b0926db353746847cfbde5903278
pepo4343 <ptangwongkit@gmail.com>
2020-07-12 21:05:28 +0700

    edit app.js

--- nbiotplatform/udpserver/app.js
+++ nbiotplatform/udpserver/app.js
@@ -3,6 +3,20 @@ const serverudp = dgram.createSocket("udp4");
 const path = require("path");
 const express = require("express");
 const http = require("http");
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+const csvWriterNbiot = createCsvWriter({
+  path: './csv/nbiot.csv',
+  append:true,
+  header: [
+    {id: 'PM1_0', title: 'PM1.0'},
+    {id: 'PM2_5', title: 'PM2.5'},
+    {id: 'PM10_0', title: 'PM10.0'},
+    {id: 'Lat', title: 'Latitude'},
+    {id: 'Lng', title: 'Longitude'},
+    {id: 'timestamp', title: 'Timestamp'},
+  ]
+});
 
 
 const port = 80;
@@ -52,14 +66,26 @@ serverudp.on("error", (err) => {
   server.close();
 });
 
-serverudp.on("message", (msg, rinfo) => {
+serverudp.on("message", async (msg, rinfo) =>  {
   console.log(`server got: ${msg} from123 ${rinfo.address}:${rinfo.port}`);
   const json = JSON.parse(msg.toString());
+
   console.log(json);
   if(json.type == "PM"){
     if(json.dev_id==="0001"){ //nbiot
+      await csvWriterNbiot.writeRecords([
+        {
+          "PM1_0":json.PM1_0.toString(),
+          "PM2_5":json.PM2_5.toString(),
+          "PM10_0":json.PM10_0.toString(),
+          "Lat":json.Lat.toString(),
+          "Lng":json.Lng.toString(),
+          "timestamp":new Date(Date.now()).toISOString(),
+        }
+      ])
+    }else{
 
-    }else{}
+    }
   }
   io.emit("message", { msg: msg.toString() });
 
